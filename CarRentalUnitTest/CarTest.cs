458f48fb131d4907063abcfd407d615d03ac8547
alvinsusanto88 <54254718+alvinsusanto88@users.noreply.github.com>
2020-03-18 22:55:09 +0700

    Initial Commit
    Initial Commit

---
+++ CarRentalUnitTest/CarTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarRental;
+using System.Linq;
+
+namespace CarRentalUnitTest
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void Test_InitializeData()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            Assert.IsTrue(mainForm.Cars.Count() == 4);
+        }
+
+        [TestMethod]
+        public void Test_CalculateInitializeData()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            double initializeRentValue = mainForm.CalculateRent();
+            Assert.AreEqual(initializeRentValue, 0);
+        }
+
+        [TestMethod]
+        public void Test_CalculateDataLessThan3Days()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            mainForm.Cars[0].DayRent = 2;
+            mainForm.Cars[0].Rent = 100000;
+            double initializeRentValue = mainForm.CalculateRent();
+            Assert.AreEqual(initializeRentValue, 200000);
+        }
+
+        [TestMethod]
+        public void Test_CalculateDataMoreThan3Days()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            mainForm.Cars[0].DayRent = 4;
+            mainForm.Cars[0].Rent = 100000;
+            double initializeRentValue = mainForm.CalculateRent();
+            Assert.AreEqual(initializeRentValue, 380000);
+        }
+
+        [TestMethod]
+        public void Test_CalculateRent2Cars()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            mainForm.Cars[0].DayRent = 2;
+            mainForm.Cars[0].Rent = 100000;
+            mainForm.Cars[1].DayRent = 2;
+            mainForm.Cars[1].Rent = 50000;
+            double initializeRentValue = mainForm.CalculateRent();
+            Assert.AreEqual(initializeRentValue, 270000);
+        }
+
+        [TestMethod]
+        public void Test_CalculateRent2CarsWithOldCar()
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.InitializeData();
+            mainForm.Cars[0].DayRent = 2;
+            mainForm.Cars[0].Rent = 100000;
+            mainForm.Cars[0].Year = 2005;
+            mainForm.Cars[1].DayRent = 2;
+            mainForm.Cars[1].Rent = 50000;
+            double initializeRentValue = mainForm.CalculateRent();
+            Assert.IsTrue(initializeRentValue < 270000);
+        }
+    }
+}
---
+++ CarRentalUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarRentalUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarRentalUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2bccb601-129d-4dc9-9a7d-e140abe7d923")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
